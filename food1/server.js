ceb3b225b70bc4cf6309f894f89c941b00d1258d
jayakumar <146693424+JayaKumar200@users.noreply.github.com>
2025-05-02 12:14:33 +0530

    Update server.js

--- food1/server.js
+++ food1/server.js
@@ -8,7 +8,14 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
 
 app.get('/', (req, res) => {
     res.send('Hi, it’s Jayakumar');
