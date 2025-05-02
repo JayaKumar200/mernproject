1f8d7e481b9524b8746e05ac68a3dc45040ae161
jayakumar <146693424+JayaKumar200@users.noreply.github.com>
2025-05-02 15:34:27 +0530

    Update Login.jsx

--- food/src/pages/Login.jsx
+++ food/src/pages/Login.jsx
@@ -41,7 +41,7 @@ const Login = () => {
       }
    
       dispatch(setLogin(loginData));
-      const login = await axios.post('https://mernproject-1-m8dp.onrender.com/login',loginData
+      const login = await axios.post('https://mern-pro-mg8d.onrender.com/login',loginData
     //     ,{
     //     headers: {
     //     Authorization: `Bearer ${token}`, 
